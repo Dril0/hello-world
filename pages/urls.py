a0ab78ec6097cde11b55898c0556cf95bfa62747
Jeremias <jeremias.acurso@hotmail.com>
2023-01-02 08:55:52 -0300

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *  # importa de .views todas las funciones
 
 urlpatterns = [
-    path("", homePageview, name="home"),
+    path("", homePageview, name="home"),  # rr
 ]
